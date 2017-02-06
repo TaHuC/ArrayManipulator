472e9caf164d9e1b0921e5daf9dca6e6849479c8
TaHuC <TaHuC@hotmail.com>
2017-02-07 00:18:20 +0200

    Finish

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ArrayManipulator
+{
+	public class MainClass
+	{
+		public static void Main()
+		{
+			List<int> numbers = Console.ReadLine()
+			                     .Split()
+			                     .Select(int.Parse)
+			                     .ToList();
+
+			var bombSpec = Console.ReadLine()
+								  .Split();
+
+			int bomb = int.Parse(bombSpec[0]);
+			int power = int.Parse(bombSpec[1]);
+
+			for (var i = 0; i < numbers.Count; i++)
+			{
+				if (numbers[i] == bomb)
+				{
+					var left = Math.Max(i - power, 0);
+					var right = Math.Min(i + power, numbers.Count - 1);
+
+					var lenght = right - left + 1;
+
+					numbers.RemoveRange(left, lenght);
+
+					i = 0;
+				}
+			}
+
+			Console.WriteLine(numbers.Sum());
+		}
+	}
+}
